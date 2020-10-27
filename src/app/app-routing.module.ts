b45894ee83af0e4a866e9bd6623092ffd72c9061
cepragamihail <yahim09@gmail.com>
2020-10-27 14:39:28 +0200

    add rout guard and login authentication logic

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,11 @@
 import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
+import { DashboardComponent } from "./dashboard/dashboard.component";
 import { LoginComponent } from "./login/login.component";
+import { AuthGuard } from "./_helpers/auth.guard";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/login", pathMatch: "full" },
+  { path: '', component: DashboardComponent, canActivate: [AuthGuard] },
   { path: "login", component: LoginComponent },
   // otherwise redirect to home
   { path: "**", redirectTo: "" }
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from "@angular/core";
 import { FormGroup, FormBuilder, Validators } from "@angular/forms";
+import { ActivatedRoute, Router } from "@angular/router";
+import { first } from "rxjs/operators";
+import { AuthenticationService } from "../_services/authentication.service";
 
 @Component({
   selector: "app-login",
@@ -7,15 +10,58 @@ import { FormGroup, FormBuilder, Validators } from "@angular/forms";
   styleUrls: ["./login.component.css"]
 })
 export class LoginComponent implements OnInit {
-  constructor(private formBuilder: FormBuilder) {}
-  loginForm: FormGroup = this.formBuilder.group({
-    userName: [, { validators: [Validators.required], updateOn: "change" }],
-    password: [, { validators: [Validators.required], updateOn: "change" }]
-  });
+  loginForm: FormGroup;
+  loading = false;
+  submitted = false;
+  returnUrl: string;
+  error = "";
 
-  ngOnInit() {}
+  constructor(
+    private formBuilder: FormBuilder,
+    private route: ActivatedRoute,
+    private router: Router,
+    private authenticationService: AuthenticationService
+  ) {
+    // redirect to home if already logged in
+    if (this.authenticationService.userValue) {
+      this.router.navigate(["/"]);
+    }
+  }
+
+  ngOnInit() {
+    this.loginForm = this.formBuilder.group({
+      userName: [, { validators: [Validators.required], updateOn: "change" }],
+      password: [, { validators: [Validators.required], updateOn: "change" }]
+    });
+    // get return url from route parameters or default to '/'
+    this.returnUrl = this.route.snapshot.queryParams["returnUrl"] || "/";
+  }
+
+  // convenience getter for easy access to form fields
+  get f() {
+    return this.loginForm.controls;
+  }
 
   onSubmit() {
-    console.log("login");
+    this.submitted = true;
+
+    // stop here if form is invalid
+    if (this.loginForm.invalid) {
+      return;
+    }
+
+    this.loading = true;
+    this.authenticationService
+      .login(this.f.username.value, this.f.password.value)
+      .pipe(first())
+      .subscribe(
+        data => {
+          this.router.navigate([this.returnUrl]);
+        },
+        error => {
+          this.error = error;
+          this.loading = false;
+        }
+      );
   }
 }
