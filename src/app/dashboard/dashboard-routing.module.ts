5df420fea4763e03d08c469e90d1b62df4723dbc
cepragamihail <yahim09@gmail.com>
2020-11-03 09:16:04 +0200

    fix inject resource error

--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from "@angular/core";
 import { Routes, RouterModule } from "@angular/router";
 import { EditSiteComponent } from "../components/edit-site/edit-site.component";
 import { SitesComponent } from "../components/sites/sites.component";
+import { DashboardComponent } from "./dashboard.component";
 
 const routes: Routes = [
   {
     path: "",
-    redirectTo: "site",
-    pathMatch: "full"
+    component: DashboardComponent
   },
   {
     path: "site",
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,47 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
+
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatCardModule } from "@angular/material/card";
+import { MatInputModule } from "@angular/material/input";
+import { MatButtonModule } from "@angular/material/button";
+import { MatDividerModule } from "@angular/material/divider";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatIconModule } from "@angular/material/icon";
+import { MatListModule } from "@angular/material/list";
+import { FlexLayoutModule } from "@angular/flex-layout";
+
 import { NavComponent } from "./nav/nav.component";
 
 @NgModule({
-  imports: [CommonModule],
+  imports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule,
+    FlexLayoutModule
+  ],
   declarations: [NavComponent],
-  exports: [NavComponent, CommonModule]
+  exports: [
+    CommonModule,
+    MatFormFieldModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatInputModule,
+    MatButtonModule,
+    MatDividerModule,
+    MatMenuModule,
+    MatIconModule,
+    MatListModule,
+    NavComponent,
+    FlexLayoutModule
+  ]
 })
 export class SharedModule {}
